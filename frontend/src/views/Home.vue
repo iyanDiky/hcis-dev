6c4adcc407c0ed569e055e961a2f87ab4847f4ad
Dicky Yanor <141791772+iyanDiky@users.noreply.github.com>
2026-05-20 23:58:32 +0800

    feat: Add Laravel backend and Vue frontend in Docker

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+  <div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="page-title-box">
+          <h4 class="page-title">Dashboard</h4>
+        </div><!--end page-title-box-->
+      </div><!--end col-->
+    </div>
+    
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Welcome to HCIS Dashboard</h5>
+            <p class="card-text">This is a sample page sliced from Metrica v3.1</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
